cb5e0a57329818c5a09582ddf10c4bc6eb936163
aKai <fahim.ak24@gmail.com>
2024-03-14 16:05:34 +0600

    some responsive done

--- src/js/t&c.js
+++ src/js/t&c.js
@@ -1,16 +1,6 @@
-// Adding "header" element
-fetch("./src/components/header.html")
-	.then((response) => response.text())
-	.then((data) => {
-		document.querySelector("main").insertAdjacentHTML("beforebegin", data);
-	});
-
 // Adding "footer" element
 fetch("./src/components/footer.html")
 	.then((response) => response.text())
 	.then((data) => {
 		document.querySelector("main").insertAdjacentHTML("afterend", data);
 	});
-
-
-	
